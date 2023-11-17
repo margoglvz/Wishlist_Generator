4bae1dfa68a26d3789d895305ed95f3d348d6700
gracecluvohio <94275517+gracecluvohio@users.noreply.github.com>
2023-11-16 20:50:31 -0800

    back end
    made back end

---
+++ script.js
@@ -0,0 +1,16 @@
+function search() {
+    let query = document.getElementById('user-input').value;
+
+    let show = document.getElementById('show').value;
+
+    let color = document.getElementById('color').value;
+
+    // fetch("http://127.0.0.1:5000/get/" + query).then(res => res.json()).then(response => {
+    //     document.getElementById('display').innerHTML = JSON.stringify(response);
+    // })\
+
+    
+    fetch("http://127.0.0.1:5000/get/" + show + ' ' + color).then(res => res.json()).then(response => {
+        document.getElementById('display').innerHTML = JSON.stringify(response);
+    })
+}
